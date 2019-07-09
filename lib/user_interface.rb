6c5d2e2ec4440ecbdd6a7cba145a8fda65e90161
github username <github email address>
2019-07-09 16:07:31 +0100

    change blog_name to title, & added user_interface

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -34,7 +34,7 @@ class UserInterface
             next_phase
         when "Go to Main Menu"
             main_menu
-        when "Exit"
+        when "Quit"
             exit
         end
 
@@ -80,7 +80,6 @@ class UserInterface
     
         story_title = prompt.ask("What the title of your story")
         story_content = prompt.ask("Write your magical story")
-        # binding.pry
         #add and save to db
         new_story = Story.create(:title=>"#{story_title}", :content=>"#{story_content}", :user_id=>"#{@current_user.id}", :location_id=>"#{single_location.id}") 
         main_menu
@@ -89,13 +88,93 @@ class UserInterface
 
     
     def main_menu
+        system "clear"
+        main_menu = prompt.select("Please select from the following", "Your Stories", "Location Stories", "Quit")
+
+        case main_menu
+
+        when "Your Stories"
+            user_stories
+        when "Location Stories"
+            location_stories
+        when "Quit"
+            exit
+        end
+
         #this will have user
         # 1.user_story_edit # ??
         # 2. user_story_delete #destroy
         # 3. Location story # add a hint find_by name
         # 4.exit
     end
+  
+    def option_drop_down(user_all_stories_title)
+        select_story = prompt.select("Pick a story", [user_all_stories_title])
+        story_option = prompt.select("Would you like to edit or delete this story", ["Edit", "Delete", "Go back"])
+        story = Story.find_by(title: select_story)
+       
+        user_content = @current_user.all_user_content
+        content = Story.find_by(content: user_content)
 
+        if story_option == "Edit"
+            user_content = prompt.ask "Edit your story"
+            story.update(content: user_content)
+            prompt.ok("Your story has now been updated")
+        # add story title choice with a boolean. 
+        sleep 1
+        main_menu
+        
+        elsif story_option == "Delete"
+            Story.destroy(story.id)
+            prompt.ok("Story has now been deleted, back to Main Menu")
+            sleep 1
+            main_menu
+        else story_option == "Go Back"
+            main_menu
+        end
+    end
+
+
+
+    def user_stories
+        user_all_stories = Story.where(user_id: @current_user).select { |story| story.title != "" }
+        user_all_stories_title = user_all_stories.map{|story| story.title}
+        # @current_user.stories.each do |story| 
+        #     puts story.title
+        #  end
+
+        if user_all_stories_title.count > 0 
+        option_drop_down(user_all_stories_title) 
+       
+        else
+            puts "You have 0 stories"
+            sleep 1
+            main_menu
+        end
+    end
+
+
+
+    def location_stories 
+        # Location.all_location_names
+        list_of_locations = Story.all.map { |story| story.location }.uniq.sort
+        location_array = Story.all.map{|story| story.location.name}.uniq #returns individual locations name
+        # all_titles = Story.story_title
+        # all_titles.select{|stor| story.}
+        chosen_location = prompt.select("Choose location", location_array)
+        stories_of_location = Location.find_by(name: chosen_location).stories
+        location_stories = stories_of_location.map { |story| story.title}
+        puts location_stories
+
+        
+
+    end
+
+
+            
+
+
+        
     # def story_index
     #     puts "--- story by you, #{@current_user.name} ---"
     #     # How to print out each story by user with an ID and a Title.
@@ -122,6 +201,6 @@ class UserInterface
     # def user_input
     #     self.last_input = @prompt
     # end
-binding.pry
+
 
 end 
