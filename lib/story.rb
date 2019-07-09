f7d7d4a3fe6a7aae8b7ac5400c73f9e660a886bd
github username <github email address>
2019-07-09 09:27:46 +0100

    added create story

--- lib/story.rb
+++ lib/story.rb
@@ -1,4 +1,15 @@
 class Story < ActiveRecord::Base
     belongs_to :user
     belongs_to :location
+
+    def self.all_stories
+       Story.all
+    end
+
+    def self.story_title
+        all_stories.map{|story| story.title}
+    end
+
+
+
 end 
